23e0131a19d773a294c66fbdcd62671e404ad111
karlis pcwork <karlita.valdivieso@gmail.com>
2021-07-21 23:31:27 -0400

    funcion para solicitar items (costooo)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
 // 5. AGREGAR EL PRODUCTO AL CARRITO con push
 // 6. MOSTRAR EL RESULTADO POR CONSOLE con reduce.
 
-//consructor productos
 
 let cart = []; // carrito vacio
 
@@ -18,7 +17,7 @@ class Item {
         this.stock = parseInt(stock);
         this.descr = descr;
         this.foto = foto;
-        this.itemNum= parseInt(itemNum);
+        this.itemNum = parseInt(itemNum);
     }
 
 }
@@ -68,17 +67,42 @@ const stockTienda = [{
     }
 ];
 
-//producto agregado ira en el carrito 
 
-class CartItem {//crea un subitem del carrito ejemplo "2 x poleras x 3000 pesos"
+
+class CartItem { //crea un subitem del carrito ejemplo "2 x poleras x 3000 pesos"
     constructor(item, qty) {
-        this.nombre = item.nombre;//saca el nombre del item guardado en el stock tienda
-        this.precio =item.precio;
-        this.itemNum = item.itemNum;//codigo para encontrar el item
+        this.nombre = item.nombre; //saca el nombre del item guardado en el stock tienda
+        this.precio = item.precio;
+        this.itemNum = item.itemNum; //codigo para encontrar el item
         this.qty = qty;
         this.subtotal = item.precio * qty //subtotal del item individual
 
     }
 }
 
+//crea lista de items disponibles en tienda
+let itemList = stockTienda.map(el => el.nombre);//crea un listado solo con los  nombres pasando el array como elemento (eso es "el") y busca la propiedad nombre
+console.log(itemList);
 
+//variables de las funciones
+let qtyItem=parseInt(); //cantidad del item
+let cartItem; //item en si
+let getNewItem; // agregaras mas items
+let addedItem; //items ya agregados
+
+//proceso de compra
+
+//elegir un item
+
+function addItem() {
+    do {
+        cartItem= prompt("elije uno de estos productos: " + itemList);
+        qtyItem = prompt("cuantos quieres?: ")
+    }
+    while (cartItem === "" || isNaN(cartItem) !== true || cartItem === null) 
+
+}
+addItem();
+
+console.log(cartItem)
+console.log(qtyItem)
